55c937c6c8fc670ba101e81aaad041bed791f5ad
nem.web <nemchand.student.eee23@itbhu.ac.in>
2025-06-26 23:03:41 +0530

    Refactor ListBookings component to handle null items gracefully in booking list

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -50,24 +50,27 @@ const ListBookings = () => {
             </tr>
           </thead>
           <tbody className="text-sm font-light">
-            {bookings.map((item, index) => (
-              <tr
-                key={index}
-                className="border-b border-primary/20 bg-primary/5 even:bg-primary/10"
-              >
-                <td className="p-2 min-w-45 pl-5">{item.user.name}</td>
-                <td className="p-2 min-w-45 pl-5">{item.show.movie.title}</td>
-                <td className="p-2">{dateFormat(item.show.showDateTime)}</td>
-                <td className="p-2">
-                  {Object.keys(item.bookedSeats)
-                    .map((seat) => item.bookedSeats[seat])
-                    .join(", ")}
-                </td>
-                <td className="p-2">
-                  {currency} {item.amount}
-                </td>
-              </tr>
-            ))}
+            {bookings.map((item, index) => {
+              if (!item || !item.show || !item.user) return null;
+              return (
+                <tr
+                  key={index}
+                  className="border-b border-primary/20 bg-primary/5 even:bg-primary/10"
+                >
+                  <td className="p-2 min-w-45 pl-5">{item.user.name}</td>
+                  <td className="p-2 min-w-45 pl-5">{item.show.movie.title}</td>
+                  <td className="p-2">{dateFormat(item.show.showDateTime)}</td>
+                  <td className="p-2">
+                    {Object.keys(item.bookedSeats)
+                      .map((seat) => item.bookedSeats[seat])
+                      .join(", ")}
+                  </td>
+                  <td className="p-2">
+                    {currency} {item.amount}
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
